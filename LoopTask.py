3e33caa7976336d055fbc011215c36bd13157f45
Josie Howker <61244560+jmghowker@users.noreply.github.com>
2020-02-23 14:28:47 +0000

    Loop Task
    Create a Python application that will loop between 1 and 100
    • The numbers are to be printed out alongside their squared value
    • The app should stop when a squared value of 200 or more is reached
    • Reconfigure the application to take in a user value to produce
    squared values up to

---
+++ LoopTask.py
@@ -0,0 +1,29 @@
+number = int(input("Enter a number between 1 and 100: "))
+print(number * number)
+
+
+
+for i in range(2,100):
+    print(i , i*i)
+    if i*i >= 200:
+        break
+
+# prints all squared values up to user inputted value  
+for i in range(number):
+    print(i*i)
+
+
+
+
+
+ 
+
+
+
+
+
+    
+
+
+
+    
